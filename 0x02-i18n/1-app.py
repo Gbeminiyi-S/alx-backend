28684f5321a302891989fe657616333f092c8cd8
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-09-11 05:57:38 +0000

    update to render HTML

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -20,4 +20,4 @@ app.config.from_object(Config)
 @app.route('/')
 def index_page():
     """This function defines a route for the root URL ('/')"""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
