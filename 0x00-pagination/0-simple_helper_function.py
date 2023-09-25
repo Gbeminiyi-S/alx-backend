b465e9c210aec1b6429f9a0df99af2b3a5bc217a
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-09-25 08:02:06 +0000

    adding extra line before function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """This module defines the function `index_range`"""
 
+
 def index_range(page, page_size):
     """Returns a tuple of size two containing a start index and an end index"""
     start_index = page_size * (page - 1)
